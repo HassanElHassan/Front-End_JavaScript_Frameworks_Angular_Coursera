b6a2ee33ae553b4b84592abe7b7494a1d784b19b
HassanElHassan <aboemohammad84@gmail.com>
2021-01-11 00:21:48 +0100

    Components Part 2

--- firstAngular/src/app/app.module.ts
+++ firstAngular/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { HammerModule} from '@angular/platform-browser';
 import {MatListModule} from '@angular/material/list'
+import {MatGridListModule} from '@angular/material/grid-list'
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'
 
 import { MenuComponent } from './menu/menu.component'
 
@@ -20,7 +23,10 @@ import { MenuComponent } from './menu/menu.component'
     FlexLayoutModule,
     MatToolbarModule,
     // HammerModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
